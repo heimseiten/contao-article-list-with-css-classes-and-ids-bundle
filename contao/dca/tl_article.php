cfb476555a562baaf523c809f8a61baa3a8b01bd
Niels <info@heimseiten.de>
2022-11-06 12:03:33 +0100

    Update tl_article.php

--- contao/dca/tl_article.php
+++ contao/dca/tl_article.php
@@ -1,3 +1,3 @@
 <?php
 $GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'][] = 'cssID';
-$GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <span class="article_id_classes">%3$s</span>';
+$GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <div class="article_id_classes">%3$s</div>';
