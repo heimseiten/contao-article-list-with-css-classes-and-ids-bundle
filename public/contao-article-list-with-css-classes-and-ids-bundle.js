56cefffe3298abe6f727136d08db27670b0245e2
Niels <info@heimseiten.de>
2022-11-06 11:59:04 +0100

    Update contao-article-list-with-css-classes-and-ids-bundle.js

--- public/contao-article-list-with-css-classes-and-ids-bundle.js
+++ public/contao-article-list-with-css-classes-and-ids-bundle.js
@@ -1,6 +1,6 @@
 document.addEventListener( 'DOMContentLoaded', function () {
-    if (document.querySelector('.article_id_classes')) {
-        document.querySelectorAll('.article_id_classes').forEach(e => {
+    if (document.querySelector('.tree_view .article_id_classes')) {
+        document.querySelectorAll('.tree_view .article_id_classes').forEach(e => {
 
             const ids =e.innerHTML.substring( 0,e.innerHTML.indexOf(',') )
             const classes =e.innerHTML.substring( e.innerHTML.indexOf(',') + 2, e.innerHTML.length )
@@ -11,21 +11,21 @@ document.addEventListener( 'DOMContentLoaded', function () {
             let idsInnerHtml = ['']
             let classesInnerHtml = ['']
             
-            arrayOfClasses.forEach(arrayElement => {
+            arrayOfClasses.sort().forEach(arrayElement => {
                 if ( arrayElement.length > 0 ) {
                     arrayElement = '<span>' + arrayElement + '</span>'
                     classesInnerHtml.push(arrayElement)
                 }
             })
             
-            arrayOfIds.forEach(arrayElement => {
+            arrayOfIds.sort().forEach(arrayElement => {
                 if ( arrayElement.length > 0 ) {
                     arrayElement = '<span>' + arrayElement + '</span>'
                     idsInnerHtml.push(arrayElement)
                 }
             })
             
-            e.innerHTML = '<div class="classes">' + classesInnerHtml.join('') + '</div><div class="ids">' + idsInnerHtml.join('') + '</div>'
+            e.innerHTML = '<div class="ids">' + idsInnerHtml.join('') + '</div><div class="classes">' + classesInnerHtml.join('') + '</div>'
 
         })
     }    
