bd0ee8673e44d56829c11fdf771cb5dcc995c467
Niels <info@heimseiten.de>
2022-11-06 07:30:20 +0100

    Create tl_article.php

---
+++ contao/dca/tl_article.php
@@ -0,0 +1,3 @@
+<?php
+$GLOBALS['TL_DCA']['tl_article']['list']['label']['fields'][] = 'cssID';
+$GLOBALS['TL_DCA']['tl_article']['list']['label']['format'] = '%s <span class="article_id_classes">%3$s</span>';
