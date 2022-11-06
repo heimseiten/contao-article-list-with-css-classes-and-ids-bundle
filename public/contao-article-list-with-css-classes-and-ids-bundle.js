8f5fa8282b67e9ae9c98c116a7afe8bfc1b3f145
Niels <info@heimseiten.de>
2022-11-06 07:50:08 +0100

    Update contao-article-list-with-css-classes-and-ids-bundle.js

--- public/contao-article-list-with-css-classes-and-ids-bundle.js
+++ public/contao-article-list-with-css-classes-and-ids-bundle.js
@@ -1,30 +1,32 @@
-if (document.querySelector('.article_id_classes')) {
-    document.querySelectorAll('.article_id_classes').forEach(e => {
+document.addEventListener( 'DOMContentLoaded', function () {
+    if (document.querySelector('.article_id_classes')) {
+        document.querySelectorAll('.article_id_classes').forEach(e => {
 
-        const ids =e.innerHTML.substring( 0,e.innerHTML.indexOf(',') )
-        const classes =e.innerHTML.substring( e.innerHTML.indexOf(',') + 2, e.innerHTML.length )
-        
-        const arrayOfIds = ids.split(' ')
-        const arrayOfClasses = classes.split(' ')
+            const ids =e.innerHTML.substring( 0,e.innerHTML.indexOf(',') )
+            const classes =e.innerHTML.substring( e.innerHTML.indexOf(',') + 2, e.innerHTML.length )
+            
+            const arrayOfIds = ids.split(' ')
+            const arrayOfClasses = classes.split(' ')
 
-        let idsInnerHtml = ['']
-        let classesInnerHtml = ['']
-        
-        arrayOfClasses.forEach(arrayElement => {
-            if ( arrayElement.length > 0 ) {
-                arrayElement = '<span>' + arrayElement + '</span>'
-                classesInnerHtml.push(arrayElement)
-            }
+            let idsInnerHtml = ['']
+            let classesInnerHtml = ['']
+            
+            arrayOfClasses.forEach(arrayElement => {
+                if ( arrayElement.length > 0 ) {
+                    arrayElement = '<span>' + arrayElement + '</span>'
+                    classesInnerHtml.push(arrayElement)
+                }
+            })
+            
+            arrayOfIds.forEach(arrayElement => {
+                if ( arrayElement.length > 0 ) {
+                    arrayElement = '<span>' + arrayElement + '</span>'
+                    idsInnerHtml.push(arrayElement)
+                }
+            })
+            
+            e.innerHTML = '<div class="classes">' + classesInnerHtml.join('') + '</div><div class="ids">' + idsInnerHtml.join('') + '</div>'
+
         })
-        
-        arrayOfIds.forEach(arrayElement => {
-            if ( arrayElement.length > 0 ) {
-                arrayElement = '<span>' + arrayElement + '</span>'
-                idsInnerHtml.push(arrayElement)
-            }
-        })
-        
-        e.innerHTML = '<div class="classes">' + classesInnerHtml.join('') + '</div><div class="ids">' + idsInnerHtml.join('') + '</div>'
-
-    })
-}    
+    }    
+})
