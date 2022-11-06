482323d6ad6f5290617ab89bc4982d135db72ced
Niels <info@heimseiten.de>
2022-11-06 07:25:43 +0100

    Update config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -4,6 +4,6 @@ use Contao\System;
 use Symfony\Component\HttpFoundation\Request;
 
 if (System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest(System::getContainer()->get('request_stack')->getCurrentRequest() ?? Request::create(''))) {
-    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoarticlelistwithcssclassesandidsbundle/contao-article-list-with-css-classes-and-ids-bundle.scss|static';
-    $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/heimseitencontaoarticlelistwithcssclassesandidsbundle/contao-article-list-with-css-classes-and-ids-bundle.js';
+    $GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaoarticlelistwithcssclassesandids/contao-article-list-with-css-classes-and-ids-bundle.scss|static';
+    $GLOBALS['TL_JAVASCRIPT'][] = 'bundles/heimseitencontaoarticlelistwithcssclassesandids/contao-article-list-with-css-classes-and-ids-bundle.js';
 }
