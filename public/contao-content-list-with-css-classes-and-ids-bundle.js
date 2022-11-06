d7c8996a588e0b1adf27bf7d52aef2b934d0fe33
Niels <info@heimseiten.de>
2022-11-06 15:40:09 +0100

    Update contao-content-list-with-css-classes-and-ids-bundle.js

--- public/contao-content-list-with-css-classes-and-ids-bundle.js
+++ public/contao-content-list-with-css-classes-and-ids-bundle.js
@@ -1,74 +1,76 @@
-const contao_classes = [
-    'media',
-    'media--above',
-    'media--right',
-    'media--left',
-    'media--below',
-    'content-code',
-    'content-headline',
-    'content-list',
-    'content-markdown',
-    'content-table',
-    'download-element',
-    'ext-jpg',
-    'content-download',
-    'content-downloads',
-    'block',
-    'content-image',
-    'content-gallery--cols-4',
-    'content-gallery',
-    'content-player',
-    'tl_gray',
-    'ce_accordionSingle',
-    'ce_accordion',
-    'ce_text',
-    'content-text' 
-]
-if (document.querySelector('.limit_height')) {
-    document.querySelectorAll('.limit_height > *').forEach(e => {
-        const arrayOfClasses = e.className.split(' ')
-        const arrayOfIds = e.id.split(' ')
-        for (i = 0; i < arrayOfClasses.length; i++) {
-            if ( contao_classes.includes(arrayOfClasses[i]) ) { 
-                arrayOfClasses.splice(i, 1);
-                i--;
-            }
-        }
-        for (i = 0; i < arrayOfIds.length; i++) {
-            if ( contao_classes.includes(arrayOfIds[i]) ) { 
-                arrayOfIds.splice(i, 1);
-                i--;
-            }
-        }
-        
-        let idsInnerHtml = ['']
-        if ( arrayOfIds.length > 0) {
-            arrayOfIds.sort().forEach(arrayElement => {
-                if ( arrayElement.length > 0 ) {
-                    arrayElement = '<span>' + arrayElement + '</span>'
-                    idsInnerHtml.push(arrayElement)
+document.addEventListener( 'DOMContentLoaded', function () {
+    const contao_classes = [
+        'media',
+        'media--above',
+        'media--right',
+        'media--left',
+        'media--below',
+        'content-code',
+        'content-headline',
+        'content-list',
+        'content-markdown',
+        'content-table',
+        'download-element',
+        'ext-jpg',
+        'content-download',
+        'content-downloads',
+        'block',
+        'content-image',
+        'content-gallery--cols-4',
+        'content-gallery',
+        'content-player',
+        'tl_gray',
+        'ce_accordionSingle',
+        'ce_accordion',
+        'ce_text',
+        'content-text' 
+    ]
+    if (document.querySelector('.limit_height')) {
+        document.querySelectorAll('.limit_height > *').forEach(e => {
+            const arrayOfClasses = e.className.split(' ')
+            const arrayOfIds = e.id.split(' ')
+            for (i = 0; i < arrayOfClasses.length; i++) {
+                if ( contao_classes.includes(arrayOfClasses[i]) ) { 
+                    arrayOfClasses.splice(i, 1);
+                    i--;
                 }
-            })
-        }
-        let classesInnerHtml = ['']
-        if ( arrayOfClasses.length > 0) {
-            arrayOfClasses.sort().forEach(arrayElement => {
-                if ( arrayElement.length > 0 ) {
-                    arrayElement = '<span>' + arrayElement + '</span>'
-                    classesInnerHtml.push(arrayElement)
+            }
+            for (i = 0; i < arrayOfIds.length; i++) {
+                if ( contao_classes.includes(arrayOfIds[i]) ) { 
+                    arrayOfIds.splice(i, 1);
+                    i--;
                 }
-            })
-        }
-    
-        var frag = document.createDocumentFragment();
-        var elem = document.createElement('div');
+            }
+            
+            let idsInnerHtml = ['']
+            if ( arrayOfIds.length > 0) {
+                arrayOfIds.sort().forEach(arrayElement => {
+                    if ( arrayElement.length > 0 ) {
+                        arrayElement = '<span>' + arrayElement + '</span>'
+                        idsInnerHtml.push(arrayElement)
+                    }
+                })
+            }
+            let classesInnerHtml = ['']
+            if ( arrayOfClasses.length > 0) {
+                arrayOfClasses.sort().forEach(arrayElement => {
+                    if ( arrayElement.length > 0 ) {
+                        arrayElement = '<span>' + arrayElement + '</span>'
+                        classesInnerHtml.push(arrayElement)
+                    }
+                })
+            }
         
-        elem.innerHTML = '<div class="ids">' + idsInnerHtml.join('') + '</div><div class="classes">' + classesInnerHtml.join('') + '</div>'
-        elem.classList.add('content_id_classes');
-        frag.appendChild(elem);
+            var frag = document.createDocumentFragment();
+            var elem = document.createElement('div');
+            
+            elem.innerHTML = '<div class="ids">' + idsInnerHtml.join('') + '</div><div class="classes">' + classesInnerHtml.join('') + '</div>'
+            elem.classList.add('content_id_classes');
+            frag.appendChild(elem);
+            
+            const toolbar = e.parentElement.parentElement.querySelector('.cte_type')
+            toolbar.appendChild(frag)
         
-        const toolbar = e.parentElement.parentElement.querySelector('.cte_type')
-        toolbar.appendChild(frag)
-    
-    })
-}
+        })
+    }
+})
